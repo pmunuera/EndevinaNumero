7f207cd1b639071f1dfb8b2baecc0c71c1889d29
pmunuera <pmunueragarcia.cf@iesesteveterradas.cat>
2022-09-16 19:18:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/endevinanumero/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.endevinanumero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+    }
+}
