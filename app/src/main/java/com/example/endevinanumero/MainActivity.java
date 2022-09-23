4dc5cfad1ac131b782bbdd24d9efdcb0507798de
pmunuera <95918506+pmunuera@users.noreply.github.com>
2022-09-23 19:25:00 +0200

    Commit 1

--- app/src/main/java/com/example/endevinanumero/MainActivity.java
+++ app/src/main/java/com/example/endevinanumero/MainActivity.java
@@ -2,20 +2,33 @@ package com.example.endevinanumero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.button_id);
+        final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                toast.show();
+
             }
         });
+
     }
 }
