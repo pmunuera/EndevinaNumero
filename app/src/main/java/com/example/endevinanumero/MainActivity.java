b5ee4785790f123600b3a45c24fde17355af9f9f
pmunuera <95918506+pmunuera@users.noreply.github.com>
2022-09-30 19:09:09 +0200

    Commit 2

--- app/src/main/java/com/example/endevinanumero/MainActivity.java
+++ app/src/main/java/com/example/endevinanumero/MainActivity.java
@@ -7,7 +7,10 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.method.ScrollingMovementMethod;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -16,9 +19,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     int contador=0;
-    int random = (int) (Math.random()*10-1);
+    int random = (int) (Math.random()*100-1);
     int number = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
         final Button button = findViewById(R.id.button);
         final String[] scrollText = {""};
+        ArrayList usuaris = new ArrayList();
+        ArrayList intents= new ArrayList();
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -36,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 number = Integer.parseInt(variable.getText().toString());
                 TextView viewIntents = (TextView)findViewById(R.id.textView);
                 TextView textScrollView = (TextView)findViewById(R.id.textView4);
+                textScrollView.setMovementMethod(new ScrollingMovementMethod());
                 Context context = getApplicationContext();
                 CharSequence text="";
                 int duration = Toast.LENGTH_SHORT;
@@ -55,18 +63,32 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else if(number==random){
                     text = "Molt be, aquest es el numero.\nIntentos: "+contador;
-                    random = (int) (Math.random()*10-1);
                     AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-
                     alertDialogBuilder.setTitle("Felicitats");
-
                     alertDialogBuilder
                             .setMessage("Has encertat, era el numero "+random+". Vols guardar?")
                             .setCancelable(false)
                             .setPositiveButton("Si",new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog,int id) {
-
                                     //Si la respuesta es afirmativa aquí agrega tu función a realizar.
+                                    AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                                    alert.setTitle("Guardar");
+                                    alert.setMessage("Introdueix el nom de l'usuari");
+                                    final EditText input = new EditText(MainActivity.this);
+                                    alert.setView(input);
+                                    alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int whichButton) {
+                                            Editable value = input.getText();
+                                            usuaris.add(input.getText());
+                                            intents.add(contador);
+                                            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                                            intent.putExtra("usuaris",usuaris);
+                                            intent.putExtra("points",intents);
+                                            startActivity(intent);
+                                            // Do something with value!
+                                        }
+                                    });
+                                    alert.create().show();
                                 }
                             })
                             .setNegativeButton("No",new DialogInterface.OnClickListener() {
@@ -74,6 +96,7 @@ public class MainActivity extends AppCompatActivity {
                                     dialog.cancel();
                                 }
                             }).create().show();
+                    random = (int) (Math.random()*100-1);
                     contador=0;
                     viewIntents.setText("Intents: 0");
                     scrollText[0]="";
---
+++ app/src/main/java/com/example/endevinanumero/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.endevinanumero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    String[] usuaris;
+    int[] intents;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
