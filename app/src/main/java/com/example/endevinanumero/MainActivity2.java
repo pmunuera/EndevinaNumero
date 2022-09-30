ce853ce0d4ea45c64b4aca09f9a4ee2f5b8b012a
pmunuera <95918506+pmunuera@users.noreply.github.com>
2022-09-30 19:14:57 +0200

    Commit 2

--- app/src/main/java/com/example/endevinanumero/MainActivity2.java
+++ app/src/main/java/com/example/endevinanumero/MainActivity2.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity2 extends AppCompatActivity {
     String[] usuaris;
     int[] intents;
@@ -14,6 +16,8 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+
 
     }
 }
