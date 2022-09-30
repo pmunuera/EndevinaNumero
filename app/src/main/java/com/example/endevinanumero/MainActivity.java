cf5fe6d0d3a49a5cbebaebee4f2968e8a1e46ab0
pmunuera <pmunueragarcia.cf@iesesteveterradas.cat>
2022-09-30 12:47:59 +0200

    Commit 2

--- app/src/main/java/com/example/endevinanumero/MainActivity.java
+++ app/src/main/java/com/example/endevinanumero/MainActivity.java
@@ -8,7 +8,9 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,61 +26,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button button = findViewById(R.id.button);
+        final String[] scrollText = {""};
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+
                 EditText variable = (EditText)findViewById(R.id.editTextNumber);
                 number = Integer.parseInt(variable.getText().toString());
                 TextView viewIntents = (TextView)findViewById(R.id.textView);
+                TextView textScrollView = (TextView)findViewById(R.id.textView4);
                 Context context = getApplicationContext();
                 CharSequence text="";
                 int duration = Toast.LENGTH_SHORT;
                 if (number<random){
                    text = "El numero es mes gran que aquest";
                     contador++;
+                    scrollText[0] = scrollText[0] +"\nNumero: "+number;
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
                 }
                 else if(number>random){
                     text = "El numero es mes petit que aquest";
                     contador++;
+                    scrollText[0] = scrollText[0] +"\nNumero: "+number;
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
                 }
                 else if(number==random){
                     text = "Molt be, aquest es el numero.\nIntentos: "+contador;
                     random = (int) (Math.random()*10-1);
-                    StartGameDialogFragment dialog = new StartGameDialogFragment(contador);
-                    //dialog.show(getSupportFragmentManager(),"Felicitats");
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+
+                    alertDialogBuilder.setTitle("Felicitats");
+
+                    alertDialogBuilder
+                            .setMessage("Has encertat, era el numero "+random+". Vols guardar?")
+                            .setCancelable(false)
+                            .setPositiveButton("Si",new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,int id) {
+
+                                    //Si la respuesta es afirmativa aquí agrega tu función a realizar.
+                                }
+                            })
+                            .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,int id) {
+                                    dialog.cancel();
+                                }
+                            }).create().show();
                     contador=0;
                     viewIntents.setText("Intents: 0");
+                    scrollText[0]="";
+                    textScrollView.setText(scrollText[0]);
                 }
+                textScrollView.setText(scrollText[0]);
                 viewIntents.setText("Intents: "+contador);
-
+                variable.setText("");
             }
         });
 
     }
-    public class StartGameDialogFragment extends DialogFragment {
-        int intents;
-        public StartGameDialogFragment(int intents){
-            this.intents=intents;
 
-        }
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the Builder class for convenient dialog construction
-            String ok = "OK";
-            String dialog_win_game = "Felicitats, has guanyat en "+intents+" intents";
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage(R.string.dialog_win_game)
-                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-
-                        }
-                    });
-            // Create the AlertDialog object and return it
-            return builder.create();
-        }
-    }
 }
