bc3188dbac1e0d367dc835451ff27f61e16d58da
pmunuera <95918506+pmunuera@users.noreply.github.com>
2022-10-03 17:02:49 +0200

    Commit TextView

--- app/src/main/java/com/example/endevinanumero/MainActivity2.java
+++ app/src/main/java/com/example/endevinanumero/MainActivity2.java
@@ -26,40 +26,22 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         final Button button2 = findViewById(R.id.button2);
         Intent intent = getIntent();
-
-
+        TextView viewRecord = (TextView)findViewById(R.id.textView3);
+        TextView viewRecordN = (TextView)findViewById(R.id.textView6);
         int count=1;
         for (Usuaris u: usuarisRecord2){
             recordsUs=recordsUs+"\n"+count+". "+u.getNom();
             recordsNum=recordsNum+"\n"+u.getIntents();
             count++;
         }
-        TableRow.LayoutParams  params1=new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT,1.0f);
-        TableRow.LayoutParams params2=new TableRow.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT);
-        TableLayout tbl=(TableLayout) findViewById(R.id.tableLayoutId);
-
-            //Creating new tablerows and textviews
-            TableRow row=new TableRow(this);
-            TextView txt1=new TextView(this);
-            TextView txt2=new TextView(this);
-
-            //setting the text
-            txt1.setText(recordsUs);
-            txt2.setText(recordsNum);
-            txt1.setLayoutParams(params1);
-            txt2.setLayoutParams(params1);
-
-            //the textviews have to be added to the row created
-            row.addView(txt1);
-            row.addView(txt2);
-
-            row.setLayoutParams(params2);
-            tbl.addView(row);
-
+        viewRecord.setMovementMethod(new ScrollingMovementMethod());
+        viewRecordN.setMovementMethod(new ScrollingMovementMethod());
+        viewRecord.setText(recordsUs);
+        viewRecordN.setText(recordsNum);
         button2.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            Intent intent2 = new Intent(MainActivity2.this, MainActivity.class);
-            startActivity(intent2);
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent2);
             }
         });
     }
